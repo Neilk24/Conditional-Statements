ded61253e8f221996448404ac102ece51f33f636
Neilk24 <neilk2050@gmail.com>
2024-09-02 17:45:35 -0400

    Add files via upload

---
+++ odd_and_even.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number: "))
+
+print("Number to be calculated odd or even: ", number)
+
+if number%2==0:
+    print("This number is even!")
+
+else:
+    print("This number is odd!")
