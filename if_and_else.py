46427cbd340587035892ba2b7eb4f7eaeb86d8bf
Neilk24 <neilk2050@gmail.com>
2024-09-02 17:34:44 -0400

    Add files via upload

---
+++ if_and_else.py
@@ -0,0 +1,8 @@
+number = int(input("Enter any number: "))
+
+if number<= 99:
+    print("I am in if block and this number is less than or equal to 99!")
+
+else: 
+    print("I am in else block and this number is greater than 99!")
+    
